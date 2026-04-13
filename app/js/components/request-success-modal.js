e2fd03b05e063d6ddb8fcf9e3b9430b3c2b3b51c
Serhiy Hourwitz <shmone800@gmail.com>
2026-04-13 18:46:25 +0300

    correct layout, style & logic of success modal

--- app/js/components/request-success-modal.js
+++ app/js/components/request-success-modal.js
@@ -1,12 +1,14 @@
 const successBtn = document.querySelector('.request-success-modal__success-btn');
 
+const closeSuccessBtn = document.querySelector('.request-success-modal__close-btn');
+
 const closeSuccessModal = () => {
   successModal.classList.remove('active');
   overlay.classList.remove('active');
   document.body.classList.remove('no-scroll');
 }
 
-closeBtn.addEventListener('click', closeSuccessModal);
+closeSuccessBtn.addEventListener('click', closeSuccessModal);
 
 successBtn.addEventListener('click', closeSuccessModal);
 
